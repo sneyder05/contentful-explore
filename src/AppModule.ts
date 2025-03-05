a6a1ac071cfde9e39f4c5744a87dbb68e1138161
Sneyder Navia <fabiansneyder@gmail.com>
2025-03-04 20:52:42 -0500

    feat: Pull products from contentful

--- src/AppModule.ts
+++ src/AppModule.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppConfigModule } from './config/ConfigService';
 import { DatabaseModule } from './database/DatabaseModule';
+import { ProductModule } from './core/product/module/ProductModule';
 
 @Module({
-  imports: [AppConfigModule, DatabaseModule],
+  imports: [AppConfigModule, DatabaseModule, ProductModule],
 })
 export class AppModule {}
---
+++ src/core/product/service/SyncProductsService.ts
@@ -0,0 +1,69 @@
+import { Inject, Injectable, Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Cron, CronExpression } from '@nestjs/schedule';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ContentfulClientApi, EntryCollection } from 'contentful';
+import { ConfigKey } from 'src/config/ConfigKey';
+import { ContentfulProduct, ContentfulProductSkeleton } from 'src/core/contentful/types/ContentProductSkeleton';
+import { CONTENTFUL_CLIENT } from 'src/core/contentful/util/ContenfulConstants';
+import { Repository } from 'typeorm';
+import { Product } from '../entity/Product';
+
+const CONTENTFUL_BATCH_SIZE = 50;
+
+@Injectable()
+export class SyncProductsService {
+  private readonly logger = new Logger(SyncProductsService.name);
+
+  constructor(
+    @Inject(CONTENTFUL_CLIENT) private readonly contentfulClient: ContentfulClientApi<any>,
+    private readonly configService: ConfigService,
+    @InjectRepository(Product) private readonly productRepository: Repository<Product>,
+  ) {}
+
+  @Cron(CronExpression.EVERY_HOUR)
+  async syncProducts() {
+    this.logger.log('Syncing products...');
+
+    await this.saveProducts(await this.fetchProducts());
+  }
+
+  private async fetchProducts(): Promise<ContentfulProduct[]> {
+    const contentType = this.configService.get<string>(ConfigKey.CONTENTFUL_CONTENT_TYPE, 'product');
+    let response: EntryCollection<ContentfulProductSkeleton>;
+    const items: ContentfulProduct[] = [];
+
+    do {
+      response = await this.contentfulClient.getEntries<ContentfulProductSkeleton>({
+        content_type: contentType,
+        limit: CONTENTFUL_BATCH_SIZE,
+        skip: items.length,
+      });
+
+      items.push(...response.items);
+    } while (response.total > items.length);
+
+    this.logger.debug(`Fetched ${items.length} products`);
+
+    return items;
+  }
+
+  private async saveProducts(products: ContentfulProduct[]): Promise<void> {
+    const entities = products.map((product) => {
+      return this.productRepository.create({
+        sku: product.fields.sku as string,
+        name: product.fields.name as string,
+        brand: product.fields.brand as string,
+        model: product.fields.model as string,
+        category: product.fields.category as string,
+        price: product.fields.price as number,
+        currency: product.fields.currency as string,
+        stock: product.fields.stock as number,
+      });
+    });
+
+    await this.productRepository.save(entities);
+
+    this.logger.debug('Products saved');
+  }
+}
