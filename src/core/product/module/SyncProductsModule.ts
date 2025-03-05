806407d096906437b5b7dfb0b58a499781e56ccd
Sneyder Navia <fabiansneyder@gmail.com>
2025-03-04 21:35:21 -0500

    feat: Sktip deleted products when syncing

--- src/core/product/module/SyncProductsModule.ts
+++ src/core/product/module/SyncProductsModule.ts
@@ -4,9 +4,11 @@ import { ScheduleModule } from '@nestjs/schedule';
 import { ContentfulModule } from 'src/core/contentful/module/ContentfulModule';
 import { Product } from '../entity/Product';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { DeletedProductsService } from '../service/DeletedProductsService';
+import { DeletedProduct } from '../entity/DeletedProduct';
 
 @Module({
-  imports: [ScheduleModule.forRoot(), TypeOrmModule.forFeature([Product]), ContentfulModule],
-  providers: [SyncProductsService],
+  imports: [ScheduleModule.forRoot(), TypeOrmModule.forFeature([Product, DeletedProduct]), ContentfulModule],
+  providers: [SyncProductsService, DeletedProductsService],
 })
 export class SyncProductsModule {}
