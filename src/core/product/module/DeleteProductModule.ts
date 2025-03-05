7a0c2a73ea553de04b3906c0b29a87fcc0412dad
Sneyder Navia <fabiansneyder@gmail.com>
2025-03-04 21:17:54 -0500

    feat: Delete product

--- src/core/product/module/DeleteProductModule.ts
+++ src/core/product/module/DeleteProductModule.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Product } from '../entity/Product';
 import { DeleteProductController } from '../controller/DeleteProductController';
+import { GetProductService } from '../service/GetProductService';
+import { DeleteProductService } from '../service/DeleteProductService';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Product])],
   controllers: [DeleteProductController],
-  providers: [],
+  providers: [GetProductService, DeleteProductService],
 })
 export class DeleteProductModule {}
